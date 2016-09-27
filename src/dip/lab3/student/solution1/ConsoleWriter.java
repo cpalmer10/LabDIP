c520e394eec50923960541a2010a2fce339e7529
cpalmer10 <cpalmer10@my.wctc.edu>
2016-09-27 14:41:02 -0500

    big changes!

--- src/dip/lab3/student/solution1/ConsoleWriter.java
+++ src/dip/lab3/student/solution1/ConsoleWriter.java
@@ -5,11 +5,27 @@ package dip.lab3.student.solution1;
  *
  * @author Chris
  */
-public class ConsoleWriter extends RandomWriter implements Writer {
+public class ConsoleWriter implements Writer {
     
     private String output;
+    private String[] tips ={
+        "An apple a day keeps the doctor away.",
+        "Always brush your teeth in the morning and night.",
+        "If you don't have something nice to say, don't say it at all.",
+        "Say cheese!",
+        "Don't text and drive.",
+        "Click it or ticket."
+    };
     
-    
+    @Override
+    public void tipOfDay(){
+        int rnd = (int) Math.floor(Math.random() * 5);
+        System.out.println(tips[rnd]);
+    }
+    @Override
+    public void welcomeMsg(){
+        System.out.println("Welcome!");
+    }
     @Override
     public void write(){
         System.out.println(output);
